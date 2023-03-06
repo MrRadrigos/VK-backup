e62717961d491aee062ea949842dbdb22216c4e7
Anton Eroshkin <amigos29@yandex.ru>
2023-03-06 22:25:02 +0300

    Fixed some bugs

--- main.py
+++ main.py
@@ -61,10 +61,12 @@ def get_photos(owner, album):
 
 
 # Ниже указываем id, profile для загрузки аватарок
-get_photos()
+account_id = int(input('Введите номер своего id: '))
+album = input('Введите откуда вы хотите скачать фото (profile - позволяет скачать аватарки): ')
+get_photos(account_id, album)
 
 
-def upload_file(file_path, ydisk_file_path, replace=False):
+def create_folder(ydisk_folder):
     with open('YD.cfg', 'r') as file:
         TOKEN = file.read().strip()
 
@@ -72,8 +74,47 @@ def upload_file(file_path, ydisk_file_path, replace=False):
         'Content-Type': 'application/json',
         'Authorization': TOKEN
     }
+
     url = 'https://cloud-api.yandex.net/v1/disk/resources'
-    res = requests.get(f'{url}/upload?path={ydisk_file_path}&overwrite={replace}', headers=headers).json()
+    ydisk_file_path = {
+        'href': f'https://cloud-api.yandex.net/v1/disk/resources',
+        'path': f'{ydisk_folder}',
+        'method': 'put',
+        'templated': 'false'
+    }
+
+    res = requests.put(url, params=ydisk_file_path, headers=headers)
+
+    if res.status_code == 201:
+        ydisk_path = res.json().get('path')
+
+        print(f'Папка "{ydisk_folder}" успешно создана')
+    else:
+        print(f'Не удалось создать папку "{ydisk_folder}". Код ответа: {res.status_code}')
+        ydisk_path = None
+
+    return ydisk_path
+
+
+# create_folder()
+
+
+def upload_file(file_path, ydisk_file_path):
+    with open('YD.cfg', 'r') as file:
+        TOKEN = file.read().strip()
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': TOKEN
+    }
+    url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+
+    params = {
+        'path': ydisk_file_path,
+        'overwrite': 'true'
+    }
+
+    res = requests.get(url, params=params, headers=headers).json()
 
     with open(file_path, 'rb') as f:
         try:
@@ -85,9 +126,16 @@ def upload_file(file_path, ydisk_file_path, replace=False):
             print(res)
 
 
+ydisk_folder = input('Введите имя папки для создания на Яндекс.Диске: ')
+create_folder(ydisk_folder)
+
 files = []
+num_photos = int(input('Введите количество фото, которое вы хотите загрузить: '))
+
 for root, directories, filenames in os.walk('photos'):
-    for filename in tqdm(filenames):
+    for el, filename in tqdm(enumerate(filenames)):
+        if el >= num_photos:
+            break
         file_path = os.path.join(os.getcwd(), 'photos', filename)
         files.append(file_path)
-        upload_file(file_path=f'{file_path}', ydisk_file_path=f'PythonTest/{filename}')
+        upload_file(file_path=f'{file_path}', ydisk_file_path=f'{ydisk_folder}/{filename}')
